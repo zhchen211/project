ed2f8c3002691b504ccbffec80447bea358360da
zhangchen <zhchen211@163.com>
2018-12-06 19:57:29 +0800

    zc

---
+++ src/js/tz.js
@@ -0,0 +1,17 @@
+$("#sy").click(function(){
+    location.href = "../index.html";
+    // $("img").attr("src","sy1.png");
+    
+})
+$("#cp").click(function(){
+    location.href = "../../html/chanpin.html";
+    
+})
+$("#jz").click(function(){
+    location.href = "../../html/jingzhun.html";
+    
+})
+$("#wo").click(function(){
+    location.href = "../../html/wode.html";
+    
+})
